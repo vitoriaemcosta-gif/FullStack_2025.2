894d3988141edd7333b19d3fd57c6fe577fe2e8a
Vitoria Emanuelly Machado Costa <vitoriae.m.costa@gmail.com>
2025-11-12 15:19:31 -0300

    cadastro

--- servidor geral/public/atividade/cadastro.js
+++ servidor geral/public/atividade/cadastro.js
@@ -6,43 +6,53 @@ app.use(bodyParser.urlencoded({extended: false }))
 app.use(bodyParser.json())
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
 var servidor = http.createServer(app);
 servidor.listen(80);
 
-console.log("Servidor rodando.......");
-
 app.get('/', function(req, res){
     res.redirect("index.html");
 
-    
 });
+
 app.get('/cadastrar', function(req, res){
     
 
     var nome = req.query.nome;
     var email = req.query.email;
     var telefone = req.query.telefone;
-    var nascimento = req.query.nascimento;
-    console.log(nome, email, telefone, nascimento);
-    res.render("resposta.ejs", { metodo: "GET",   nome, email, telefone, nascimento  });
-
+    var senha = req.query.senha;
+    console.log(nome, email, telefone, senha);
+    res.render("resposta_cad.ejs", { metodo: "GET",   nome, email, telefone, senha  });
     
 });
+
 app.post('/cadastrar', function(req, res){
     
 
     var nome = req.body.nome;
     var email = req.body.email;
     var telefone = req.body.telefone;
-    var nascimento = req.body.nascimento;
+    var senha = req.body.senha;
 
-    res.render("resposta.ejs", { metodo: "POST", nome, email, telefone, nascimento });
+    res.render("resposta_cad.ejs", { metodo: "POST", nome, email, telefone, senha });
 });
-app.get('/for', function(req, res){
+
+app.get('/logar', function(req, res){
     
+    var email = req.query.email;
+    var senha = req.query.senha;
+    console.log( email, senha);
+    res.render("resposta_cad.ejs", { metodo: "GET",   email, senha  });
+    
+});
 
-    var qtde = req.query.qtde;
+app.post('/logar', function(req, res){
+    
+    var email = req.body.email;
+    var senha = req.body.senha;
 
-    res.render("for.ejs", { qtde});
+    res.render("resposta_cad.ejs", { metodo: "POST", email, senha });
 });
